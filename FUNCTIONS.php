8ad830eb44e71c6638c6baca4dae8e2835b50b73
Fran <franciscochen.cjj@gmail.com>
2024-03-06 15:36:12 -0300

    FUNCTIONS

---
+++ FUNCTIONS.php
@@ -0,0 +1,14 @@
+<?php
+    // function happy_birthday($first_name){
+    //     echo"Happy Birthday dear {$first_name}!<br>";
+    // }
+
+    // happy_birthday("Spongebob");
+
+    function is_even($number){
+        $result = $number % 2;
+        return $result;
+    }
+
+    is_even(11);
+?>
