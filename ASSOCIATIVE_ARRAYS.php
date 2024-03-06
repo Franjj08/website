bdc2a58708009dc7d85ae4a193e0b85f044ed520
Fran <franciscochen.cjj@gmail.com>
2024-03-06 11:02:47 -0300

    Associative arrays

---
+++ ASSOCIATIVE_ARRAYS.php
@@ -0,0 +1,12 @@
+<?php
+    $capitals = array(
+        "USA"=> "WashingtonD.C",
+        "Japan"=> "Kyoto",
+        "South Korea"=> "Seoul",
+        "India"=> "New Delhi",
+    );
+
+    foreach($capitals as $key =>$value){
+        echo "{$key} {$value} <br>";
+    }
+?>
