b3041773ef0455183d5e12506908ad15f4d7ff94
Fran <franciscochen.cjj@gmail.com>
2024-03-09 19:47:09 -0300

    HASHING

---
+++ HASHING.php
@@ -0,0 +1,14 @@
+<?php
+    $password = "pizza123";
+
+    $hash = password_hash($password, PASSWORD_DEFAULT);
+
+    echo"$hash<br>";
+
+    if(password_verify("pizza123", $hash)){
+        echo "You are logged in!";
+    }
+    else{
+        echo "Incorrect password";
+    }
+?>
